54357757b379d8298d6c3acefa7dac6b4df6b040
Arslan <jacklol5760@gmail.com>
2023-12-17 12:02:01 +0600

    PWA is working from Arman repo

--- sounds/src/App.js
+++ sounds/src/App.js
@@ -1,52 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
-import React, {useEffect} from 'react';
+// App.js
+
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import { useStateProvider } from './utils/StateProvider';
 import { reducerCases } from './utils/Constants';
 import Sounds from './components/Sounds';
-import axios from 'axios';
-import { Buffer } from "buffer"
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Profile from "./components/Profile";
-import Help from "./components/Help";
+import Profile from './components/Profile';
+import Help from './components/Help';
+import { register } from './serviceWorkerRegistration';
 
-
-// Arman Krosava
 function Home() {
-  const [{token}, dispatch] = useStateProvider()
-  // var clientId = 'ec80aa53b442424096c97f3d31300c06';
-  // var clientSecret = "91fd0d69e4e54ba38a5e1393a68f631c";
+  const [{ token }, dispatch] = useStateProvider();
+
   useEffect(() => {
     const hash = window.location.hash;
-    if(hash) {
-      console.log("HASH", hash)
-      const token  = hash.substring(1).split("&")[0].split("=")[1];
-      console.log(token)
-      dispatch({type:reducerCases.SET_TOKEN, token})
+    if (hash) {
+      console.log('HASH', hash);
+      const token = hash.substring(1).split('&')[0].split('=')[1];
+      console.log(token);
+      dispatch({ type: reducerCases.SET_TOKEN, token });
     }
-  }, [token, dispatch])
+  }, [token, dispatch]);
+
   return (
     <div>
-      {
-        token ? <Sounds /> : <Login />
-      }
+      {token ? <Sounds /> : <Login />}
     </div>
-
   );
 }
 
 function App() {
+  useEffect(() => {
+    const registerServiceWorker = async () => {
+      try {
+        const registration = await navigator.serviceWorker.register('./service-worker.js');
+        console.log('Service Worker registered with scope:', registration.scope);
+      } catch (error) {
+        console.error('Service Worker registration failed:', error);
+      }
+    };
+
+    registerServiceWorker();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/help" element={<Help />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/help" element={<Help />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
 
 export default App;
--- sounds/src/index.js
+++ sounds/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { StateProvider } from './utils/StateProvider';
 import reducer, { initialState } from './utils/reducer';
+import { StateProvider } from './utils/StateProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,7 +15,33 @@ root.render(
   </React.StrictMode>
 );
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+
+// serviceWorkerRegistration.register();
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./service-worker.js')
+    .then((registration) => {
+      console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch((error) => {
+      console.error('Service Worker registration failed:', error);
+    });
+}
+
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((registration) => {
+      registration.unregister();
+    });
+  });
+}
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+reportWebVitals();
